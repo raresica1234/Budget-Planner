ef3023584ed5f0101521035c7368c6472bd038d7
ddbl <daviddeyv@gmail.com>
2021-11-05 00:49:34 +0200

    finish register page

--- budget-planner-web/src/pages/accounts/register/index.tsx
+++ budget-planner-web/src/pages/accounts/register/index.tsx
@@ -1,8 +1,31 @@
 import styles from "./register.module.scss";
 import Logo from "../../../assets/logo.svg";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { RegisterContext } from "./register-store";
+import { observer } from "mobx-react";
 
 const Register = () => {
+    const {
+        user,
+        setEmail,
+        setPassword,
+        setConfirmPassword,
+        register,
+        reset
+    } = useContext(RegisterContext);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        return reset;
+    }, [reset]);
+
+    const onClickNext = async () => {
+        await register();
+        navigate("/login");
+    } 
+    
     return (
         <div className={styles.container}>
             <div className={styles.inputsPane}> 
@@ -11,17 +34,17 @@ const Register = () => {
                     <span className={styles.title}>Sign up</span>
                     <div className={styles.input}>
                         <span className={styles.label}>Email</span>
-                        <input type="email"></input>
+                        <input type="email" value={user.email} onChange={e => setEmail(e.target.value)}></input>
                     </div>
                     <div className={styles.input}>
                         <span className={styles.label}>Password</span>
-                        <input type="password"></input>
+                        <input type="password" value={user.password} onChange={e => setPassword(e.target.value)}></input>
                     </div>
                     <div className={styles.input}>
                         <span className={styles.label}>Re-enter password</span>
-                        <input type="password"></input>
+                        <input type="password" value={user.confirmPassword} onChange={e => setConfirmPassword(e.target.value)}></input>
                     </div>
-                    <button>Next</button>
+                    <button onClick={onClickNext}>Next</button>
                 </div>
                 <div className={styles.signInContainer}>
                     <span>Already got an account? <Link to="/login">Sign in</Link></span>
@@ -35,4 +58,4 @@ const Register = () => {
     )
 }
 
-export default Register;
+export default observer(Register);
