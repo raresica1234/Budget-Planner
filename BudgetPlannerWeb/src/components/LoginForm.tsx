45ace03f2a5e442c1f597fa708c24a4de0bf7450
Vezniuc Paul <paulvezniuc@gmail.com>
2021-11-04 23:03:34 +0200

    started work on login component

---
+++ BudgetPlannerWeb/src/components/LoginForm.tsx
@@ -0,0 +1,53 @@
+import { Box, Button, Container, TextField } from '@mui/material';
+import React from 'react';
+
+export default function LoginForm() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      username: data.get('username'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+      }}>
+        <Box component="form" onSubmit={handleSubmit}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  )
+}
---
+++ budget-planner-web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import LoginForm from './components/LoginForm';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginForm/>
+    </div>
+  );
+}
+
+export default App;
