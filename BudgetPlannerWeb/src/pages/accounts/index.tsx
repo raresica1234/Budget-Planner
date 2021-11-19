aeafe879d9404765ab7f9ca0c1b06c63d0eca986
Vezniuc Paul <paulvezniuc@gmail.com>
2021-11-19 19:25:42 +0200

    made a simpler useToken

--- BudgetPlannerWeb/src/pages/accounts/index.tsx
+++ BudgetPlannerWeb/src/pages/accounts/index.tsx
@@ -1,13 +1,16 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Register from "./register";
 import Login from "./login";
+import useToken from "./useToken";
 
 const AccountRoutes = () => {
+  const { setToken } = useToken();
+
   return (
     <Routes>
       <Route path="/register" element={<Register />} />
-	  <Route path="/login" element={<Login />} />
-      <Route path="*" element={<Navigate to="/register" />} />
+      <Route path="/login" element={<Login setToken={setToken} />} />
+      <Route path="*" element={<Navigate to="/login" />} />
     </Routes>
   );
 };
--- BudgetPlannerWeb/src/pages/accounts/useToken.tsx
+++ BudgetPlannerWeb/src/pages/accounts/useToken.tsx
@@ -1,22 +1,15 @@
-import { useState } from "react";
 import { LOGIN_TOKEN } from "../../accessors/constants";
 
 
 const useToken = () => {
-  const getToken = () => {
-    return localStorage.getItem(LOGIN_TOKEN);
-  };
+  const getToken = localStorage.getItem(LOGIN_TOKEN);
 
-  const [token, setToken] = useState(getToken());
-
-  const saveToken = (token: string) => {
+  const saveToken = (token: string) => 
     localStorage.setItem(LOGIN_TOKEN, token);
-    setToken(token);
-  };
 
   return {
     setToken: saveToken,
-    token
+    token: getToken
   }
 }
 
