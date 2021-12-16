4dc25ccede64364393080a1c6b53cbd4a5b21fea
MihaiCatalin120 <mihaicatalin449@gmail.com>
2021-12-16 19:18:06 +0200

    Resolve bad indentation

--- BudgetPlannerWeb/src/pages/main/item-view/index.tsx
+++ BudgetPlannerWeb/src/pages/main/item-view/index.tsx
@@ -1,5 +1,5 @@
 import { Context, useContext, useEffect } from "react";
-import { Box, AppBar, Typography, Toolbar, IconButton, Paper , List, ListItem, ListItemText, LinearProgress} from "@mui/material";
+import { Box, AppBar, Typography, Toolbar, IconButton, Paper, List, ListItem, ListItemText, LinearProgress } from "@mui/material";
 import { Item as ItemModel } from "../../../accessors/types";
 import MenuIcon from "@mui/icons-material/Menu";
 import styles from "./items.module.scss";
@@ -21,27 +21,27 @@ const ItemsView = () => {
         <Box className={styles.mainContainer}>
             <AppBar className={styles.appBar}>
                 <Toolbar>
-					<IconButton
-						size="large"
-						edge="start"
-						color="inherit"
-						aria-label="menu"
-						className={styles.menuIconButton}
-					>
-						<MenuIcon />
-					</IconButton>
-					<Typography variant="h6" component="div" className={styles.appTitle}>
-						Budget Planner
-					</Typography>
-					<img className={styles.logo} alt="Logo" src={Logo} />
-				</Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        className={styles.menuIconButton}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" component="div" className={styles.appTitle}>
+                        Budget Planner
+                    </Typography>
+                    <img className={styles.logo} alt="Logo" src={Logo} />
+                </Toolbar>
             </AppBar>
             <Box className={styles.titleBar}>
                 <Typography variant="h6" component="div" className={styles.listTitle}>
                     {listName}
                 </Typography>
-			</Box>
-            
+            </Box>
+
             <Paper elevation={0}>
                 {isLoading && (
                     <LinearProgress />
@@ -50,8 +50,8 @@ const ItemsView = () => {
                     {items.map((item: ItemModel) => (
                         <li>
                             <ListItem>
-                                <ListItemText primary={item.name} secondary={item.price}/>
-                                <ListItemText primary={"Created at: " + new Date(item.createdAt).toLocaleString('en-GB')} secondary={"Updated at: " + new Date(item.updatedAt).toLocaleString('en-GB')} className={styles.rightSideItem}/>
+                                <ListItemText primary={item.name} secondary={item.price} />
+                                <ListItemText primary={"Created at: " + new Date(item.createdAt).toLocaleString('en-GB')} secondary={"Updated at: " + new Date(item.updatedAt).toLocaleString('en-GB')} className={styles.rightSideItem} />
                             </ListItem>
                         </li>
                     ))}
@@ -62,7 +62,7 @@ const ItemsView = () => {
                 <Typography variant="h6" component="div" className={styles.listTitle}>
                     Cost of all items: {sum}
                 </Typography>
-			</Box>
+            </Box>
         </Box>
     )
 };
