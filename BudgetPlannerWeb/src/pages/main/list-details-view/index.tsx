254733f5002ccf4d015febe29b5cd4e2086ce047
Alex Copindean <alexcopindean@yahoo.com>
2021-12-18 20:34:55 +0200

    Adjust item update

--- BudgetPlannerWeb/src/pages/main/list-details-view/index.tsx
+++ BudgetPlannerWeb/src/pages/main/list-details-view/index.tsx
@@ -1,6 +1,17 @@
 import { useContext, useEffect } from "react";
-import { Box, AppBar, Typography, Toolbar, IconButton, Paper, List, ListItem, ListItemText, LinearProgress } from "@mui/material";
-import { Item as ItemModel } from "../../../accessors/types";
+import {
+    Box,
+    AppBar,
+    Typography,
+    Toolbar,
+    IconButton,
+    Paper,
+    List,
+    ListItem,
+    ListItemText,
+    LinearProgress
+} from "@mui/material";
+import { Item } from "../../../accessors/types";
 import MenuIcon from "@mui/icons-material/Menu";
 import styles from "./list-details.module.scss";
 import { useParams } from "react-router-dom";
@@ -8,15 +19,28 @@ import Logo from "../../../assets/logo.svg";
 import { ListDetailsViewContext } from "./list-details-view-store";
 import { observer } from "mobx-react";
 import AddItemButton from "../item-view/add-item-button";
+import EditItemDialog from "../item-view/edit-item-dialog";
+import { UpdateItemContext } from "../item-view/update-item-store";
 
 const ListDetailsView = () => {
-    const { isLoading, listName, items, sum, fetchListDetails, reset } = useContext(ListDetailsViewContext);
+    const {
+        isLoading,
+        listName,
+        items,
+        sum,
+        fetchListDetails,
+        reset
+    } = useContext(ListDetailsViewContext);
+    const { openDialog, closeDialog, item } = useContext(UpdateItemContext);
     const { id } = useParams();
     
     useEffect(() => {
         fetchListDetails(id || "");
         return reset;
     }, [fetchListDetails, id, reset]);
+
+    const handleItemClick = ({ id, name, price }: Item) =>
+        openDialog({ id, listId: "", name, price });
 
     return (
         <Box className={styles.mainContainer}>
@@ -48,9 +72,11 @@ const ListDetailsView = () => {
                     <LinearProgress />
                 )}
                 <List>
-                    {items.map((item: ItemModel) => (
+                    {items.map(item => (
                         <li>
-                            <ListItem>
+                            <ListItem 
+                                style={{ cursor: "pointer" }}
+                                onClick={() => handleItemClick(item)}>
                                 <ListItemText primary={item.name} secondary={item.price} />
                                 <ListItemText 
                                     primary={`Created at: ${item.createdAt.toLocaleString()}`} 
@@ -69,6 +95,10 @@ const ListDetailsView = () => {
                 </Typography>
             </Box>
             <AddItemButton className={styles.addButton} listId={id || ""}/>
+            <EditItemDialog
+                listId={id || ""}
+                onClose={closeDialog}
+                item={item} />
         </Box>
     )
 };
