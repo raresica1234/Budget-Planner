e7b550e4e83cb1e0626eab1b49fe2173b9747b35
Alex Copindean <alexcopindean@yahoo.com>
2021-12-18 18:12:21 +0200

    Remove unused code

--- BudgetPlannerWeb/src/pages/main/list/list/shared-lists-view-store.ts
+++ BudgetPlannerWeb/src/pages/main/list/list/shared-lists-view-store.ts
@@ -1,16 +1,9 @@
 import ListsViewStore from "./components/lists-view-store";
-import {getSharedLists} from "../../../../accessors/list-accessor";
-import {makeObservable} from "mobx";
-import {createContext} from "react";
+import { getSharedLists } from "../../../../accessors/list-accessor";
+import { createContext } from "react";
 
 export class SharedListsViewStore extends ListsViewStore {
     protected fetchListsEndpoint = getSharedLists;
-
-    constructor() {
-        super();
-        makeObservable(this, {});
-    }
-
 }
 
 export const sharedListsViewStore = new SharedListsViewStore();
